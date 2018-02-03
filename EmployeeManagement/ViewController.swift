8f1db589538b4829a8b4c08e9087ac42a2994325
Leo Moon <leom@skysecure.com>
2018-02-03 14:30:05 -0800

    Initial Commit

---
+++ EmployeeManagement/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmployeeManagement
+//
+//  Created by Leo Moon on 2018-02-03.
+//  Copyright © 2018 Leo Moon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
